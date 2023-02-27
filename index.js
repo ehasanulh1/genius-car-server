20976f2ce235042fa351e78a8d142c82e26ca517
ehasanulh1 <ehasanulh1@gmail.com>
2023-02-27 14:55:11 +0600

    setup basic server

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(express.json());
+app.use(cors());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.w1kojzp.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello from genius car server')
+});
+
+app.listen(port, () => {
+    console.log(`Listening to port ${port}`)
+})
